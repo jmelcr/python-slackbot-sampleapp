649a0a49ebce0ed4083a69389e90c9f010868862
Josef Melcr <jmelcr@gmail.com>
2023-06-06 21:28:38 +0200

    fix openAI sending messages double times

--- app.py
+++ app.py
@@ -107,7 +107,8 @@ def message(payload):
                     )
                 response = completion['choices'][0]['message']['content']
                 #print("ChatGPT Response=>",chatbot_res)
-                return slack_web_client.chat_postMessage(channel=channel_id,text=response)
+                slack_web_client.chat_postMessage(channel=channel_id,text=response)
+                return message
 
             
 
